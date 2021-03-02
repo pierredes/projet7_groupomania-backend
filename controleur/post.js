6a70cbb5bbca3560303fb17e346d9fad58ac769f
pierredes <pierredesombre.pro@gmail.com>
2021-03-02 16:09:00 +0100

    change update method & add include on get all method

--- controleur/post.js
+++ controleur/post.js
@@ -1,29 +1,34 @@
-const post = require('../modele/post')
+const post = require('../modele/post');
+const utilisateur = require ('../modele/user');
 
 exports.creationPost = (req, res, next) => {
     post.create({
         titre: req.body.titre,
         sujet: req.body.sujet,
-        contenu: req.body.contenu
+        contenu: req.body.contenu,
+        user_id: req.body.user_id
     })
     .then(() => res.status(201).json({ message: 'post créer' }))
     .catch(erreur => res.status(400).json({ erreur }))
 }
 
 exports.modificationPost = (req, res, next) => {
-    post.findOne({ where: {id: req.params.id} })
-    .then(() => {
-       post.update({ 
+    // post.findOne({ where: {id: req.params.id} })
+    // .then(() => {
+       post.update({
             titre: req.body.titre,
             sujet: req.body.sujet,
-            contenu: req.body.contenu
+            contenu: req.body.contenu,
+        },
+        {
+            where: {id : req.params.id}
         })
        .then(() => {
            res.status(201).json({ message: 'post modifié'})
        })
-       .catch(error => res.status(404).json({ error }));
-    })
-    .catch(error => res.status(404).json({ error }));
+       .catch(error => res.status(404).json({ message: 'blablabla' + error }));
+    // })
+    // .catch(error => res.status(404).json({ error }));
 }
 
 exports.supressionPost = (req, res, next) => {
@@ -45,7 +50,7 @@ exports.recuperationUnPost = (req, res, next) => {
 }
 
 exports.recuperationTousLesPost = (req, res, next) => {
-    post.findAll()
+    post.findAll({include: [{model: utilisateur}]})
     .then(post => res.status(201).json({ post }))
     .catch(error => res.status(404).json({ error }));
 }
