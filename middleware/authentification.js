e56de8f07416f6f048b51c129fc32d08f99f3a95
pierredes <pierredesombre.pro@gmail.com>
2021-03-02 16:06:41 +0100

    remove splitting header

--- middleware/authentification.js
+++ middleware/authentification.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req,res,next) => {
     try {
-        const token = req.headers.authorization.split(' ')[1];
+        const token = req.headers.authorization;
         const decodeToken = jwt.verify(token, 'STRHCYSHFXGJCVHXXFGhsdfyhfcvhdfxcgf15242414hfcwgd');
         const userId = decodeToken.userId;
         if(req.body.userId && req.body.userId !== userId) {
