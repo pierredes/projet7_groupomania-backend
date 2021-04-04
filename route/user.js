e21134521ec54f258a9ff47f46cf7f42a919c79f
pierredes <pierredesombre.pro@gmail.com>
2021-04-04 15:21:26 +0200

    add auth on route

--- route/user.js
+++ route/user.js
@@ -3,10 +3,12 @@ const express = require ('express');
 const router = express.Router();
 
 const userControlleur = require('../controleur/user');
+const authentification = require('../middleware/authentification');
 
-router.get('/:id', userControlleur.recupererUnUtilisateur);
-router.get('/', userControlleur.recupererTousLesUtilisateurs)
-router.put('/modification/:id', userControlleur.modifierCompte);
-router.delete('/supression/:id', userControlleur.supprimerCompte);
+
+router.get('/:id' , userControlleur.recupererUnUtilisateur);
+router.get('/', authentification , userControlleur.recupererTousLesUtilisateurs)
+router.put('/modification/:id', authentification , userControlleur.modifierCompte);
+router.delete('/supression/:id', authentification , userControlleur.supprimerCompte);
 
 module.exports = router;
