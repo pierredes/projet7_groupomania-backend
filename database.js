7fecfae9362c0dedbc93ac6da2b6d98c01ed5fbf
pierredes <pierredesombre.pro@gmail.com>
2021-04-06 17:38:41 +0200

    add env variable

--- database.js
+++ database.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('groupomania', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.db_name, process.env.db_user, process.env.db_password, {
+    host: process.env.db_host,
     dialect: 'mysql',
     logging: false,
 });
